5e72c3aeff775c716e616c1c942b34295d0ffcaa
Gustavo P <gpedr56@gmail.com>
2023-03-02 22:37:04 -0300

    :sparkles: feat: Single kitchen request impl

--- gusfood/src/main/java/com/gplf/gusfood/api/controller/KitchenController.java
+++ gusfood/src/main/java/com/gplf/gusfood/api/controller/KitchenController.java
@@ -3,10 +3,7 @@ package com.gplf.gusfood.api.controller;
 import com.gplf.gusfood.domain.model.Kitchen;
 import com.gplf.gusfood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +17,10 @@ public class KitchenController {
     @GetMapping
     public List<Kitchen> list() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Kitchen find(@PathVariable Long id) {
+        return repository.findById(id).get();
     }
 }
